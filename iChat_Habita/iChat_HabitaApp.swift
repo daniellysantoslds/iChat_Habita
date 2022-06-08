01555b93c98c9ac83753798eb34d50ef743b99a4
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-06-08 14:19:52 -0300

    updates

--- iChat_Habita/iChat_HabitaApp.swift
+++ iChat_Habita/iChat_HabitaApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct iChat_HabitaApp: App {
+    
+    
+    init(){
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             SingnInView()
