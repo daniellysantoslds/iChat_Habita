e7a5d543652007695052b34ee929d0da73e29e4a
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2022-06-10 14:33:26 -0300

    create ContentViewModel

---
+++ iChat_Habita/Preview Content/ContentViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  ContentViewModel.swift
+//  iChat_Habita
+//
+//  Created by Danielly Santos Lopes da Silva on 10/06/22.
+//
+
+import SwiftUI
+import FirebaseAuth
+
+
+
+
+class  ContentViewModel: ObservableObject {
+    
+    
+}
